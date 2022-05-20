fc261101c1e0ac730e0896e9dd1b206c9b2f138d
HenilJShah <henilshah79@gmail.com>
2022-05-20 09:15:42 -0700

    DeSerializationApi project upload

--- Apiroot/urls.py
+++ Apiroot/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('SerializertionApi.urls')),
+    path('create/', include('DeSerializationApi.urls')),
 ]
